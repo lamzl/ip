8c2ad3978bcc6696e60500e0e647ec2d169ec298
ethanlamcode <137771880+ethanlamcode@users.noreply.github.com>
2026-01-17 22:05:56 +0800

    Level-0 Rename Greet Exit

---
+++ src/main/java/Ethanbot.java
@@ -0,0 +1,12 @@
+public class Ethanbot {
+    public static void main(String[] args) {
+        String chatBotName = "Ethanbot";
+        String line = "--------------------------------";
+        System.out.println(line);
+        System.out.println("Hello! I'm " + chatBotName);
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.print(line);
+    }
+}
