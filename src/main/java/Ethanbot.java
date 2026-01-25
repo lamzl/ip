8b32e31c5a895aeae47f31ff960b002bd340d62c
ethanlamcode <137771880+ethanlamcode@users.noreply.github.com>
2026-01-25 22:05:57 +0800

    Level-1

--- src/main/java/Ethanbot.java
+++ src/main/java/Ethanbot.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
 public class Ethanbot {
     public static void main(String[] args) {
         String chatBotName = "Ethanbot";
         String line = "--------------------------------";
+        Scanner newScan = new Scanner(System.in);
+
+        // Initial Output
         System.out.println(line);
         System.out.println("Hello! I'm " + chatBotName);
         System.out.println("What can I do for you?");
+        System.out.println(line);
+
+        // While loop to print whatever the user types, until user types "bye"
+        while(true){
+            String input = newScan.nextLine();
+            if (input.equals("bye")){
+                break;
+            }
+            else{
+                System.out.println(line);
+                System.out.println(input);
+                System.out.println(line);
+            }
+        }
+        // ending message
         System.out.println(line);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.print(line);
