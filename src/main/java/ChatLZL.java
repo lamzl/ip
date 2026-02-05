bd13f71c4a568ae324ef9d38913bc9769df1c83c
ethanlamcode <137771880+ethanlamcode@users.noreply.github.com>
2026-02-05 12:19:51 +0800

    fixes for coding standard violations

--- src/main/java/ChatLZL.java
+++ src/main/java/ChatLZL.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
 public class ChatLZL {
-    public static void printAddTaskMessage(Task task, int count){
+    public static void printAddTaskMessage(Task task, int count) {
         String line = "--------------------------------";
         System.out.println(line);
         System.out.println("Got it. I've added this task:");
         System.out.println(" " + task.toString());
         if (count == 1){
             System.out.println("Now you have " + count + " task in your list.");
-        }
-        else{
+        } else {
             System.out.println("Now you have " + count + " tasks in your list.");
         }
         System.out.println(line);
@@ -36,55 +35,51 @@ public class ChatLZL {
         System.out.println(line);
 
         // While loop to print whatever the user types, until user types "bye"
-        while(true){
+        while(true) {
             String input = newScan.nextLine();
             String[] sections = input.split(" ");
             String instruction = sections[0];
 
-            if (input.equals("bye")){
+            if (input.equals("bye")) {
                 break;
-            }
-            else if (input.equals("list")){
+            } else if (input.equals("list")) {
                 System.out.println(line);
                 if (taskCount == 1) {
                     System.out.println("Here is the task in your list:");
-                }
-                else {
+                } else {
                     System.out.println("Here are the tasks in your list:");
                 }
                 for (int i = 0; i < taskCount; ++i){
                     System.out.println((i + 1) + "." + task[i].toString()); // polymorphism
                 }
                 System.out.println(line);
-            }
-            else if (instruction.equals("mark")){
+            } else if (instruction.equals("mark")) {
                 task[Integer.parseInt(sections[1]) - 1].setDone(true);
 
                 System.out.println(line);
                 System.out.println("Nice! I've marked this task as done:");
                 System.out.println("  " + task[Integer.parseInt(sections[1]) - 1].toString());
                 System.out.println(line);
-            }
-            else if (instruction.equals("unmark")){
+            } else if (instruction.equals("unmark")) {
                 task[Integer.parseInt(sections[1]) - 1].setDone(false);
 
                 System.out.println(line);
                 System.out.println("OK, I've marked this task as not done yet:");
                 System.out.println(" [ ] " + task[Integer.parseInt(sections[1]) - 1].toString());
                 System.out.println(line);
-            } else if (instruction.equals("todo")){
+            } else if (instruction.equals("todo")) {
                 String description = input.substring(5).trim();
                 task[taskCount] = new Todo(description);
                 taskCount++;
                 printAddTaskMessage(task[taskCount - 1], taskCount);
-            } else if (instruction.equals("deadline")){
+            } else if (instruction.equals("deadline")) {
                 int byIndex = input.indexOf("/by");
                 String description = input.substring(9, byIndex).trim();
                 String by = input.substring(byIndex + 4).trim();
                 task[taskCount] = new Deadline(description, by);
                 taskCount++;
                 printAddTaskMessage(task[taskCount - 1], taskCount);
-            } else if (instruction.equals("event")){
+            } else if (instruction.equals("event")) {
                 int toIndex = input.indexOf("/to");
                 int fromIndex = input.indexOf("/from");
                 String description = input.substring(6, fromIndex).trim();
