e34bb1a31d4f683d6e69beabb3dbb1fcaa777c1a
ethanlamcode <137771880+ethanlamcode@users.noreply.github.com>
2026-02-23 13:01:40 +0800

    Extract Storage, Ui, Parser, and TaskList classes for A-MoreOOP

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class TaskList {
+    private ArrayList<Task> tasks;
+
+    public TaskList(ArrayList<Task> loadedTasks){
+        this.tasks = loadedTasks;
+    }
+
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public Task deleteTask(int index) {
+        return tasks.remove(index);
+    }
+
+    public Task getTask(int index) {
+        return tasks.get(index);
+    }
+
+    public int getSize() {
+        return tasks.size();
+    }
+
+    public boolean isEmpty() {
+        return tasks.isEmpty();
+    }
+
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
+}
---
+++ src/main/java/Ui.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Ui {
+    private static String LINE = "-------------------------------";
+    private Scanner scanner;
+
+    public Ui() {
+        this.scanner = new Scanner(System.in);
+    }
+    public void printWelcome() {
+        String logo = "   _____ _           _   _    ______ _      \n"
+                + "  / ____| |         | | | |  |___  /| |     \n"
+                + " | |    | |__   __ _| |_| |     / / | |     \n"
+                + " | |    | '_ \\ / _` | __| |    / /  | |     \n"
+                + " | |____| | | | (_| | |_| |____/ /__| |____ \n"
+                + "  \\_____|_| |_|\\__,_|\\__|______|_____|______|";
+        System.out.println("Hello from\n" + logo);
+        System.out.println(LINE);
+        System.out.println("Hello! I'm ChatLZL");
+        System.out.println("What is up?");
+        System.out.println(LINE);
+    }
+
+    public String readCommand () {
+        return scanner.nextLine();
+    }
+
+    public void printLine() {
+        System.out.println(LINE);
+    }
+
+    public void printExit() {
+        System.out.println(LINE);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+        scanner.close();
+    }
+
+    public void showError(String message) {
+        printLine();
+        System.out.println(message);
+        printLine();
+    }
+
+    public void showMessage(String message) {
+        System.out.println(message);
+    }
+
+    public boolean hasNextCommand() {
+        return scanner.hasNextLine();
+    }
+}
